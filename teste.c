43f6bb3b36782394946627f9a680f813a033e4ce
Marcelo M. Sobral <msobral@ifsc.edu.br>
2024-02-28 14:42:49 -0300

    Simplificadas as macros para facilitar uso do munit: - define_test(func_name): declara uma função de teste - create_suite(suite_var, name, num_tests, test1, test2, ...): cria uma suite de teste na variável "suite_var", e com nome "name", composta por "num_tests" testes, cujas funções são fornecidas em seguida

--- teste.c
+++ teste.c
@@ -5,82 +5,17 @@
 #include "testes.h"
 #include <unistd.h>
 
-MunitResult test1(const MunitParameter params[], void* user_data_or_fixture) {
-    munit_assert_int(2, ==, 2);
-    sleep(1);
+define_test(test1) {
+    assert_int(2, ==, 2);
     return MUNIT_OK;
 }
 
-//MunitTest testes[] = {
-//        {
-//            "/Teste1",
-//            test1,
-//            NULL,
-//            NULL,
-//            MUNIT_TEST_OPTION_NONE,
-//            NULL
-//        },
-//        { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
-//};
+define_test(test2) {
+    assert_int(1, ==, 2);
+    return MUNIT_OK;
+}
 
-#define create_test1(name, func) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
-
-#define create_test2(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test1(__VA_ARGS__)
-
-#define create_test3(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test2(__VA_ARGS__)
-
-#define create_test4(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test3(__VA_ARGS__)
-
-#define create_test5(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test4(__VA_ARGS__)
-
-#define create_test6(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test5(__VA_ARGS__)
-
-#define create_test7(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test6(__VA_ARGS__)
-
-#define create_test8(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test7(__VA_ARGS__)
-
-#define create_test9(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test8(__VA_ARGS__)
-
-#define create_test10(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test9(__VA_ARGS__)
-
-#define create_test11(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test10(__VA_ARGS__)
-
-#define create_test12(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test11(__VA_ARGS__)
-
-#define create_test13(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test12(__VA_ARGS__)
-
-#define create_test14(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test13(__VA_ARGS__)
-
-#define create_test15(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test14(__VA_ARGS__)
-
-#define create_test16(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test15(__VA_ARGS__)
-
-#define create_test17(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
-create_test16(__VA_ARGS__)
-
-#define create_test18(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
-create_test17(__VA_ARGS__)
-
-#define create_test19(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
-create_test18(__VA_ARGS__)
-
-#define create_test20(name, func, ...) {name, func, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
-create_test19(__VA_ARGS__)
-
-#define create_tests(name, ntests, ...) MunitTest name[] = { \
-create_test##ntests(__VA_ARGS__),\
-{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }}
-
-create_tests(testes, 1, "/teste1", test1);
-
-create_suite(suite, "/testes", testes);
-//static const MunitSuite suite = {
-//        "/testes", /* name */
-//        testes, /* tests */
-//        NULL, /* suites */
-//        1, /* iterations */
-//        MUNIT_SUITE_OPTION_NONE /* options */
-//};
+create_suite(suite, testes, 2, test1, test2);
 
 int main(int argc, char* argv[]) {
     /* Use µnit here. */
--- testes.h
+++ testes.h
@@ -5,34 +5,63 @@
 #ifndef LIBPRG_TESTES_H
 #define LIBPRG_TESTES_H
 
-//
-// Created by msobral on 27/02/2024.
-//
-
+#define MUNIT_ENABLE_ASSERT_ALIASES
 #include "munit.h"
-//#include <unistd.h>
-//
-//MunitResult test1(const MunitParameter params[], void* user_data_or_fixture) {
-//    munit_assert_int(2, ==, 2);
-//    sleep(1);
-//    return MUNIT_OK;
-//}
-//
-//MunitTest testes[] = {
-//        {
-//                "/Teste1",
-//                test1,
-//                      NULL,
-//                            NULL,
-//                                  MUNIT_TEST_OPTION_NONE,
-//                                                          NULL
-//        },
-//        { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
-//};
 
-#define create_suite(var, name, tests) static const MunitSuite var = { \
-        name, /* name */ \
-        tests, /* tests */ \
+#define define_test(name) MunitResult name(const MunitParameter params[], void* user_data_or_fixture)
+
+#define create_test1(name) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
+
+#define create_test2(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test1(__VA_ARGS__)
+
+#define create_test3(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test2(__VA_ARGS__)
+
+#define create_test4(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test3(__VA_ARGS__)
+
+#define create_test5(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test4(__VA_ARGS__)
+
+#define create_test6(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test5(__VA_ARGS__)
+
+#define create_test7(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test6(__VA_ARGS__)
+
+#define create_test8(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test7(__VA_ARGS__)
+
+#define create_test9(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test8(__VA_ARGS__)
+
+#define create_test10(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test9(__VA_ARGS__)
+
+#define create_test11(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test10(__VA_ARGS__)
+
+#define create_test12(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test11(__VA_ARGS__)
+
+#define create_test13(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test12(__VA_ARGS__)
+
+#define create_test14(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test13(__VA_ARGS__)
+
+#define create_test15(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test14(__VA_ARGS__)
+
+#define create_test16(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, create_test15(__VA_ARGS__)
+
+#define create_test17(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
+create_test16(__VA_ARGS__)
+
+#define create_test18(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
+create_test17(__VA_ARGS__)
+
+#define create_test19(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
+create_test18(__VA_ARGS__)
+
+#define create_test20(name, ...) {"::" #name, name, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}, \
+create_test19(__VA_ARGS__)
+
+#define create_tests(name, ntests, ...) MunitTest name[] = { \
+create_test##ntests(__VA_ARGS__),\
+{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }}
+
+#define create_suite(var, name, ntests, ...) create_tests(name, ntests, __VA_ARGS__); \
+static const MunitSuite var = { \
+        #name, /* name */ \
+        name, /* tests */ \
         NULL, /* suites */ \
         1, /* iterations */ \
         MUNIT_SUITE_OPTION_NONE /* options */ \
